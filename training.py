62855c271fbf7b193a36caba50ad0f9c64c2a2bf
magicdragon1101-ravi <magicdragon1101@proton.me>
2024-05-23 11:24:53 -0400

    Added Retriever and Generator, and more training material.
    Using a ParentDocumentRetriever to retrieve and split child documents to handle large texts.
    Using OpenAI's API to generate an output from a prompt while using the training material as context.

--- training.py
+++ training.py
@@ -49,15 +49,72 @@ for i in range(len(texts)):
         # Specify the directory where you want these
         persist_directory='./LLM_train_embedding/Doc'
     )
-    # Pushes these into the directory
-    vectordb.persist()
 
-# The built-in ChromaDb LangChain method for vector store-based retrieval
-# need two arguments, the algorithm to use and search kwargs, which specify
-# the parameters for each algorithm. k tells amount of documents to return etc
+# langchain ParentDocumentRetrieval Packages
+from langchain.retrievers import ParentDocumentRetriever
+from langchain.storage import InMemoryStore
 
-# Defines the retriever
-retriever = vectordb.as_retriever(search_type='mmr', search_kwargs ={'k':1})
+# Tells how to split the children
+child_splitter = TokenTextSplitter(chunk_size=250, chunk_overlap=10)
+# Tells how to split the parent
+parent_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=50)
 
-# Gets the document for the retriever
-retriever.get_relevant_documents('What is a Sankey?')
+vectorstore = Chroma(
+    collection_name="full_documents", embedding_function=OpenAIEmbeddings(api_key=openai_api_key)
+)
+# Creates a document store in memory
+store = InMemoryStore()
+# This retriever takes vector store and document store
+retriever = ParentDocumentRetriever(
+    vectorstore=vectorstore,
+    docstore=store,
+    child_splitter=child_splitter,
+    parent_splitter=parent_splitter
+)
+
+# Adds the child documents and splits them.
+for i in range(len(data)):
+    retriever.add_documents(data[i])
+
+# Search over documents:
+retriever.invoke("Mon endroit préféré")
+retriever.invoke("Un ŒIL dans l'espace")
+retriever.invoke("L'Incident")
+
+from operator import itemgetter
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+
+# This is the prompt I used
+
+# It takes in the documents as {context} and user provide {topic}
+template = """Mimic the writing style in the context:
+{context} and produce a blog on the topic
+
+Topic: {topic}
+
+
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+model = ChatOpenAI(api_key=openai_api_key)
+
+# Using LangCHain LCEL to supply the prompt and generate output
+chain = (
+    {
+        "context":itemgetter("topic") | retriever,
+        "topic": itemgetter("topic"),
+
+    }
+    | prompt
+    | model
+    | StrOutputParser()
+)
+# running the Chain
+docs = chain.invoke({"topic": "École"})
+
+print(docs)
